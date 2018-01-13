7967dc8c69d3e545421daf37700e0ceafbc7762d
Jarvis Zhaowei Wu <z235wu@edu.uwaterloo.ca>
2018-01-13 13:01:46 -0500

    Initial Commit

---
+++ Tackle/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tackle
+//
+//  Created by Zhaowei Wu on 2018-01-13.
+//  Copyright © 2018 Jarvis Zhaowei Wu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
