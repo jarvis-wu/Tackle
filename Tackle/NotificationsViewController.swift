f5cfe611aae5282fbf588a2669dc8aba2d955556
Jarvis Zhaowei Wu <z235wu@edu.uwaterloo.ca>
2018-01-14 21:56:02 -0500

    Added a Notifications tab and VC

---
+++ Tackle/NotificationsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationsViewController.swift
+//  Tackle
+//
+//  Created by Zhaowei Wu on 2018-01-14.
+//  Copyright © 2018 Jarvis Zhaowei Wu. All rights reserved.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
