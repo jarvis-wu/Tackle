c0e76984d13089df9ee05af94e00ae32b9cde02b
Jarvis Zhaowei Wu <z235wu@edu.uwaterloo.ca>
2018-02-06 18:34:28 -0500

    Bug fix: signout triggered at wrong index path

--- Tackle/MeViewController.swift
+++ Tackle/MeViewController.swift
@@ -92,8 +92,8 @@ class MeViewController: UIViewController, UITableViewDelegate, UITableViewDataSo
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        if indexPath.section == 1 {
-            if indexPath.row == 0 {
+        if indexPath.section == 2 {
+            if indexPath.row == 3 {
                 showSignOutAlert()
             }
         }
